ff40701a9fa1018bdfd678ba6f492dfcb1b800ef
pNagi <nagisama.j@gmail.com>
2017-08-30 18:01:29 +0700

    :green_heart: install and auth gcloud

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,7 @@
 # Exit on any error
 set -e
 
-curl https://sdk.cloud.google.com | bash
-exec -l $SHELL
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/firsttry-178217/go-circleci-docker-kube
+sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-# kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
-
-docker run --rm -v "$PWD":/usr/src/myapp -w /usr/src/myapp golang:1.8 go test
+# kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"go-circleci-docker-kube","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
