db8882841fc271343003b2c4b4346a61e411198a
pNagi <nagisama.j@gmail.com>
2017-08-30 16:01:43 +0700

    :green_heart: build image

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@
 # Exit on any error
 set -e
 
-sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/firsttry-178217/go-circleci-docker-kube
+docker build -f Dockerfile -t gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1 .
+sudo /opt/google-cloud-sdk/bin/gcloud docker push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-# kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"docker-hello-google","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
+# kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"go-circleci-docker-kube","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
