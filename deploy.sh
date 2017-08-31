de7ade4861e0501d0a1f23b922c37b94014bda34
pNagi <nagisama.j@gmail.com>
2017-08-31 17:08:48 +0700

    :green_heart:

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,5 @@
 # Exit on any error
 set -e
 
-sudo ${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
+gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 kubectl patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
