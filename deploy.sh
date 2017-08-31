47add56245273383b5a93fa2cc06f196f58f11f5
pNagi <nagisama.j@gmail.com>
2017-08-31 15:46:48 +0700

    :green_heart: add gcloud alias

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,6 @@
 # Exit on any error
 set -e
 
-
-sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
+sudo gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
 kubectl patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
