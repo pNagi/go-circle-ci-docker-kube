939e0e7c870bedd9f311621dc6a2d72d2cf1332c
pNagi <nagisama.j@gmail.com>
2017-08-31 17:17:50 +0700

    :green_heart:

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,5 @@
 # Exit on any error
 set -e
 
-gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
-kubectl patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
+${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
+${HOME}/google-cloud-sdk/bin/kubectl patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
