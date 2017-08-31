a4e459c840ce3e71f3848ce65b098016ffe7142d
pNagi <nagisama.j@gmail.com>
2017-08-31 15:31:48 +0700

    :green_heart: set cluster and patch deployment

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ set -e
 
 sudo /opt/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-# kubectl patch deployment docker-hello-google -p '{"spec":{"template":{"spec":{"containers":[{"name":"go-circleci-docker-kube","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
+kubectl patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
