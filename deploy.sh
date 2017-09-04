fcc8071907f42e98502faf776c230bf1b2fa5139
pNagi <nagisama.j@gmail.com>
2017-09-04 19:05:25 +0700

    :green_heart: change deployment name

--- deploy.sh
+++ deploy.sh
@@ -5,4 +5,4 @@ set -e
 
 ${HOME}/google-cloud-sdk/bin/gcloud docker -- push gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1
 ${HOME}/google-cloud-sdk/bin/gcloud container images add-tag gcr.io/firsttry-178217/go-circleci-docker-kube:$CIRCLE_SHA1 gcr.io/firsttry-178217/go-circleci-docker-kube:latest
-${HOME}/google-cloud-sdk/bin/kubectl --namespace dev patch deployment hello-world -p '{"spec":{"template":{"spec":{"containers":[{"name":"hello-world","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
+${HOME}/google-cloud-sdk/bin/kubectl --namespace dev patch deployment go-deployment -p '{"spec":{"template":{"spec":{"containers":[{"name":"go-deployment","image":"gcr.io/firsttry-178217/go-circleci-docker-kube:'"$CIRCLE_SHA1"'"}]}}}}'
